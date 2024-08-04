bfa822263d4bb375fe9c00fd0b767a387a104c5a
Maciej Loch <maqloch@gmail.com>
2024-08-04 16:08:24 +0200

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>App1</title>
+
+        <!-- Fonts -->
+
+        <!-- Styles -->
+        <style>
+        </style>
+    </head>
+    <body class="">
+        <div>Hello there {{$name}}</div>
+    </body>
+</html>
