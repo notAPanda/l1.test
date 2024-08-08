4e226db6b01721291d4092d33d6b6b6d34a6421b
Maciej Loch <maqloch@gmail.com>
2024-08-08 09:48:35 +0200

    fix scheduler

--- routes/console.php
+++ routes/console.php
@@ -1,11 +1,13 @@
 <?php
 
+
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
 
-Artisan::command('app:create-user')->purpose('generate test user')->everyMinute();
+Schedule::command('app:create-user')->everyMinute();
