b888b38f305d7719c5e4ceafeb1fd2da594ed327
Maciej Loch <maqloch@gmail.com>
2024-08-08 13:41:09 +0200

    command test

---
+++ app/Console/Commands/ClearUsers.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class ClearUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:clear-users';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        DB::table('users')->delete();
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -4,9 +4,5 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-// Artisan::command('inspire', function () {
-//     $this->comment(Inspiring::quote());
-// })->purpose('Display an inspiring quote')->hourly();
-
-
 Schedule::command('app:create-user')->everyMinute();
+Schedule::command('app:clear-users')->hourly();
