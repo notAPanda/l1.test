26769baf1ee0cbbebb81378deec6a3f330d2847a
Maciej Loch <maqloch@gmail.com>
2024-08-08 08:05:51 +0200

    test scheduler

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:create-user';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+
+        $name = Str::random(8);
+
+        User::create([
+            'name' => $name,
+            'email' => $name . '@test.pl',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
