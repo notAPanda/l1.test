b3b2e858f25ae313c33497f0ffd01c84eca0e889
Maciej Loch <maqloch@gmail.com>
2024-08-14 07:51:30 +0200

    inertia test

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,47 @@
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+
+defineProps({
+    users: {
+        type: Array,
+    },
+});
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2
+                class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"
+            >
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div
+                    class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg"
+                >
+                    <div class="p-6 lg:p-8">
+                        <h1 class="text-2xl">Hello there!</h1>
+                        <p
+                            class="mt-6 text-gray-500 dark:text-gray-400 leading-relaxed"
+                        >
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Ad adipisci nesciunt sint deleniti recusandae,
+                            cum excepturi neque possimus qui sapiente quibusdam
+                            iusto itaque vitae, ab provident nam dignissimos.
+                            Repudiandae, eos?
+                        </p>
+                        <ul class="mt-6 font-mono">
+                            <li v-for="user in users">
+                                {{ user.id }} - {{ user.email }}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,50 +1,27 @@
 <?php
 
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-
-    $users = App\Models\User::all();
-
-    return view('welcome', [
-        'name' => 'Maciek',
-        'users' => $users,
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
     ]);
 });
 
-Route::get('/seed', function () {
-    $test_user = [
-        'name' => 'Maciek',
-        'email' => 'test@test.pl',
-        'password' => bcrypt('password'),
-    ];
-
-    $user = App\Models\User::where('email', $test_user['email'])->first();
-
-    if (!$user) {
-        return App\Models\User::create($test_user);
-    }
-
-    return 'ok';
-});
-
-Route::get('/redis/set', function() {
-    Cache::set('test', 'hello');
-
-    return response()->json([
-        'status' => 'ok',
-    ]);
-});
-
-Route::get('/redis', function () {
-    return response()->json([
-        'value_from_cache' => Cache::get('test'),
-    ]);
-});
-
-
-Route::get('/cache', function () {
-    return response()->json(DB::table('cache')->get());
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard', [
+            'users' => User::all(),
+        ]);
+    })->name('dashboard');
 });
